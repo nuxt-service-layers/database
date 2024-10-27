45fd2e551cd0c7d2d1a70bf97382fdd2b207a394
Cody Wakeford <codypwakeford@gmail.com>
2024-10-27 15:48:56 +0000

    added support for services.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,65 +1,26 @@
-import path from "path"
-import type ServiceConfig from "../base/types/serviceConfig"
-let serviceLayerConfig: ServiceConfig
+//TODO: Make this function global
+import { getServicesConfig } from "../base/modules/configModule"
 
-try {
-	serviceLayerConfig = require(path.join(process.cwd(), "services.config.ts"))
-} catch (error) {
-	serviceLayerConfig = {} as ServiceConfig
-}
-
-const firestoreDbLayer: [string, { install: boolean }] = [
-	// "github:nuxt-service-layers/firebase#master",
-	"../firebase",
-	{ install: true },
-]
-const mongoDbLayer: [string, { install: boolean }] = [
-	// "github:nuxt-service-layers/mongo#master",
-	"../mongo",
-	{ install: true },
-]
-
-const postgresDbLayer: [string, { install: boolean }] = [
-	"github:nuxt-service-layers/postgres#master",
-	{ install: true },
-]
-const sqliteDbLayer: [string, { install: boolean }] = [
-	"github:nuxt-service-layers/sqlite#master",
-	{ install: true },
-]
-
-// Choose a nuxt layer to use.
-// This is done to ensure appropriate code splitting
-// with no uneeded deps
-function getDbLayer() {
-	switch (serviceLayerConfig.database) {
-		case "firestore":
-			return firestoreDbLayer
-
-		case "mongo":
-			return mongoDbLayer
-
-		case "postgres":
-			return postgresDbLayer
-
-		case "sqlite":
-			return sqliteDbLayer
-
-		case undefined:
-			console.warn(
-				"[database-connection] An error occured when trying to access services.config.ts database option.",
-			)
-			break
-
-		default:
-			console.warn("[database-connection] An unsupported database type was provided.")
-			break
-	}
-}
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
 	devtools: { enabled: true },
+
 	extends: [getDbLayer()],
 })
+
+/**Select the appropriate database layer */
+function getDbLayer() {
+	const servicesConfig = getServicesConfig()
+	const database = servicesConfig.database
+
+	const layers: Record<string, [string, { install: boolean }]> = {
+		firestore: ["../firebase", { install: true }],
+		mongo: ["../mongo", { install: true }],
+		postgres: ["github:nuxt-service-layers/postgres#master", { install: true }],
+		sqlite: ["github:nuxt-service-layers/sqlite#master", { install: true }],
+	}
+
+	if (layers[database]) return layers[database]
+
+	console.warn("An unsupported database type has been specified in the services.config.ts")
+}
